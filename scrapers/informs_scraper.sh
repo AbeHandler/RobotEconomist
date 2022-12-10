9046624aaed3093cc334424d139d91a166f40212
Abe Handler <akh2103@gmail.com>
2022-12-10 06:45:52 -0700

    feat: include management science scraper

--- scrapers/informs_scraper.sh
+++ scrapers/informs_scraper.sh
@@ -1,8 +1,15 @@
-for i in $(pages -b https://pubsonline.informs.org/toc/mnsc/ -m 68); do
+rm -rf mgmt.toc.txt && touch mgmt.toc.txt
+rm -rf mgmt.urls.txt && touch mgmt.urls.txt
+
+for i in $(pages -b https://pubsonline.informs.org/toc/mnsc/ -m 70); do
     for var in $(seq 1 4); do
-        echo $i"/"$var
+        echo $i"/"$var >> mgmt.toc.txt
     done
 done
 
 
-
+while read p; do 
+    echo $p |  stream -d 10 -v | urls | grep doi | uniq >> mgmt.urls.txt 
+    echo $p
+    wc -l mgmt.urls.txt
+done < mgmt.toc.txt
