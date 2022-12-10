b144c227489119ef732de977137c565cf31b1098
Abe Handler <akh2103@gmail.com>
2022-12-10 07:18:16 -0700

    refactor: change `informs_scraper` to take an informs code

--- scrapers/informs_scraper.sh
+++ scrapers/informs_scraper.sh
@@ -1,15 +1,19 @@
-rm -rf mgmt.toc.txt && touch mgmt.toc.txt
-rm -rf mgmt.urls.txt && touch mgmt.urls.txt
 
-for i in $(pages -b https://pubsonline.informs.org/toc/mnsc/ -m 70); do
+INFORMS_CODE="mnsc" # managment science
+rm -rf $INFORMS_CODE.toc.txt && touch $INFORMS_CODE.toc.txt
+rm -rf $INFORMS_CODE.urls.txt && touch $INFORMS_CODE.urls.txt
+
+for i in $(pages -b https://pubsonline.informs.org/toc/$INFORMS_CODE/ -m 70); do
     for var in $(seq 1 4); do
-        echo $i"/"$var >> mgmt.toc.txt
+        echo $i"/"$var >> $INFORMS_CODE.toc.txt
     done
 done
 
 
 while read p; do 
-    echo $p |  stream -d 10 -v | urls | grep doi | uniq >> mgmt.urls.txt 
+    echo $p |  stream -d 10 -v | urls | grep doi | uniq >> $INFORMS_CODE.urls.txt 
     echo $p
-    wc -l mgmt.urls.txt
-done < mgmt.toc.txt
+    wc -l $INFORMS_CODE.urls.txt
+done < $INFORMS_CODE.toc.txt
+
+mv $INFORMS_CODE.urls.txt management_science_paper_url_list.txt
