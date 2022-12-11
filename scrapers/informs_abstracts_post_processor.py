770c425129bd52b111b643ab01a7ee2912ad5925
Abe handler <akh2103@gmail.com>
2022-12-10 18:08:27 -0700

    k

--- scrapers/informs_abstracts_post_processor.py
+++ scrapers/informs_abstracts_post_processor.py
@@ -2,17 +2,19 @@ import json
 import os
 import sys
 from bs4 import BeautifulSoup
+from tqdm import tqdm as tqdm
 
 
-
-code = sys.argv[1] # e.g. "mnsc"
+code = sys.argv[1]  # e.g. "mnsc"
 with open(f"{code}.abstracts.jsonl", "r") as inf:
     with open(f"clean.{code}.abstracts.jsonl", "w") as of:
-        for i in inf:
+        for i in tqdm(inf):
             i = json.loads(i)
             soup = BeautifulSoup(i["html"], 'html.parser')
             text = soup.text
             id_ = i["url"].split("/").pop()
             out = {f'{code}_paper_id': id_, "abstract": text}
             of.write(json.dumps(out) + "\n")
-os.system(f"mv clean.{code}.abstracts.jsonl code.abstracts.jsonl")
+
+print(f"clean.{code}.abstracts.jsonl")
+os.system(f"mv clean.{code}.abstracts.jsonl {code}.abstracts.jsonl")
