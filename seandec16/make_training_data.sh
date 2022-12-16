6e96b0f6bdc640e0639359a538ca49968b41ee6d
Abe Handler <akh2103@gmail.com>
2022-12-16 10:10:14 -0700

    asdf

--- seandec16/make_training_data.sh
+++ seandec16/make_training_data.sh
@@ -1,2 +1,3 @@
 gunzip -c mnsc.sentences.jsonl.gz| shuf | jq .sent | grep -f seeds.txt -i -w  | shuf > yes.txt
 gunzip -c mnsc.sentences.jsonl.gz| shuf | head -1000 > no.txt
+gunzip -c mnsc.sentences.jsonl.gz| jq .sent | shuf > all_data.txt
