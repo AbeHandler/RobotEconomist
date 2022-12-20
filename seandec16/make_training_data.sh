6e1df8216516a5b4f2f9b1b97bf45051941d9363
Abe Handler <akh2103@gmail.com>
2022-12-20 05:30:51 -0700

    adding causal text.csv

--- seandec16/make_training_data.sh
+++ seandec16/make_training_data.sh
@@ -1,3 +1,4 @@
 gunzip -c mnsc.sentences.jsonl.gz| shuf | jq .sent | grep -f seeds.txt -i -w  | shuf > yes.txt
 gunzip -c mnsc.sentences.jsonl.gz| shuf | head -1000 > no.txt
+cat yes.txt no.txt | shuf > causal_text.csv
 gunzip -c mnsc.sentences.jsonl.gz| jq .sent | shuf > all_data.txt
